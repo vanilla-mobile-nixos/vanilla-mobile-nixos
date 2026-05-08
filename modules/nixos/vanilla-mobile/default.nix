26352e627f8c757803a66765512642b19477f337
June Stepp <git@junestepp.me>
2026-05-07 21:00:18 -0500

    fix(modemmanager): guard service change under mkif

--- modules/nixos/vanilla-mobile/default.nix
+++ modules/nixos/vanilla-mobile/default.nix
@@ -9,10 +9,12 @@ self:
   # Enable Modem Manager quick suspend and resume support. Without it,
   # Modem Manager will crash and not resume properly after a suspend.
   # See <https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/work_items/1039>
-  systemd.services.ModemManager.serviceConfig.ExecStart = [
-    "" # Ignore original ExecStart
-    "${pkgs.modemmanager}/bin/ModemManager --test-quick-suspend-resume"
-  ];
+  systemd.services.ModemManager.serviceConfig.ExecStart =
+    lib.mkIf config.networking.modemmanager.enable
+      [
+        "" # Ignore original ExecStart.
+        "${pkgs.modemmanager}/bin/ModemManager --test-quick-suspend-resume"
+      ];
 
   # People typically have single presses toggle the display/suspend, not a full poweroff.
   services.logind.settings = {
