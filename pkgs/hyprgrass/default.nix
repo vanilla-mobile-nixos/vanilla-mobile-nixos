38b23a573eaa8d0fd9484891fcefc0bdd7a801f7
June Stepp <git@junestepp.me>
2026-06-23 16:51:18 -0500

    fix(hyprgrass): add workspace swipe patch

--- pkgs/hyprgrass/default.nix
+++ pkgs/hyprgrass/default.nix
@@ -1,10 +1,10 @@
-# See <https://github.com/NixOS/nixpkgs/pull/526498>.
 {
   hyprgrass,
   wf-touch,
   fetchFromGitHub,
 }:
 (hyprgrass.override {
+  # See <https://github.com/NixOS/nixpkgs/pull/526498>.
   wf-touch = wf-touch.overrideAttrs {
     buildInputs = [ ];
     mesonFlags = [
@@ -12,13 +12,20 @@
     ];
   };
 }).overrideAttrs
-  rec {
-    version = "0.55.4";
+  (
+    finalAttrs: prevAttrs: rec {
+      version = "0.55.4";
 
-    src = fetchFromGitHub {
-      owner = "horriblename";
-      repo = "hyprgrass";
-      tag = "hl-${version}";
-      hash = "sha256-tCt7FNc1RBHou/ym7B0XzoOqqNq8Df+dizEDkAgJ4U0=";
-    };
-  }
+      src = fetchFromGitHub {
+        owner = "horriblename";
+        repo = "hyprgrass";
+        tag = "hl-${version}";
+        hash = "sha256-tCt7FNc1RBHou/ym7B0XzoOqqNq8Df+dizEDkAgJ4U0=";
+      };
+
+      patches = prevAttrs.patches or [ ] ++ [
+        # See <https://github.com/horriblename/hyprgrass/issues/391>.
+        ./workspace-swipe-edge.patch
+      ];
+    }
+  )
