dba9376061d7137e310ea33d2888d1a800c5a3b8
June Stepp <git@junestepp.me>
2026-07-12 11:18:51 -0500

    ci: cache kernel config files

--- pkgs/linux-kernel/default.nix
+++ pkgs/linux-kernel/default.nix
@@ -1,4 +1,10 @@
-{ pkgs }:
-{
-  linux_sdm845 = pkgs.callPackage ./sdm845 { };
-}
+{ lib, pkgs }:
+lib.concatMapAttrs
+  (name: value: {
+    ${name} = value;
+    # Cache kernel config files for <https://github.com/NixOS/nixpkgs/blob/5404c134fd2a119addaf62f1986c3e3816b6dcc5/nixos/modules/config/sysctl.nix#L73>.
+    "${name}-configfile" = value.configfile;
+  })
+  {
+    linux_sdm845 = pkgs.callPackage ./sdm845 { };
+  }
